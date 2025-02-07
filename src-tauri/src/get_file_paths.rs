b4448f3b37304cd638f1f8f2a42c253e780a15dd
Konzsldfie2343 <146638369+Konzsldfie2343@users.noreply.github.com>
2025-02-07 09:12:16 +0900

    get_file_paths.rs の更新

--- src-tauri/src/get_file_paths.rs
+++ src-tauri/src/get_file_paths.rs
@@ -26,3 +26,39 @@ fn get_paths(path: &str, is_recursive: bool) -> Result<Vec<String>, String> {
         ))
     }
 }
+
+fn get_folder_paths(folder_path: &String) -> Result<Vec<String>, String> {
+    let mut folder_paths: Vec<String> = Vec::new();
+    let entries = fs::read_dir(&folder_path).map_err(|e| e.to_string())?;
+
+    for entry in entries {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            folder_paths.push(path.to_string_lossy().to_string());
+        }
+    }
+    Ok(folder_paths)
+}
+
+
+fn get_file_paths(folder_path: &String) -> Result<Vec<String>, String> {
+    let mut file_paths: Vec<String> = Vec::new();
+    let entries = fs::read_dir(&folder_path).map_err(|e| e.to_string())?; // ここでエラー処理
+
+    for entry in entries {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let path = entry.path();
+
+        if path.is_file() {
+            if let Some(ext) = path.extension().and_then(|s| s.to_str()) {
+                let ext_lower = ext.to_lowercase();
+                if ext_lower == "png" || ext_lower == "jpg" || ext_lower == "jpeg" {
+                    file_paths.push(path.to_string_lossy().to_string());
+                }
+            }
+        }
+    }
+    Ok(file_paths) // Vec<String> を Result で返す
+}
